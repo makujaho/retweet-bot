1be944d5dbb706f81503738a0d37e52c94e123f7
mkzero <info@linux-web-development.de>
2012-06-10 00:55:24 +0200

    Added Log. Added check if already RTed.

--- bot.php
+++ bot.php
@@ -8,12 +8,18 @@
  * @subpackage twitter-bot
  */
 
-function clog ($string) { var_dump("$string");echo PHP_EOL; }
-
 require 'tmhOAuth/tmhOAuth.php';
 require 'tmhOAuth/tmhUtilities.php';
 
-require_once 'config.php';
+require 'config.php';
+
+function clog ($string) {
+	if (BotConfig::LOG_ENABLED === false) {
+		return;
+	}
+	error_log(date(('Y-m-d h:i:s')) . " [BOT]: ", 3, BotConfig::LOG_FILE_PATH);
+    error_log($string, 3, BotConfig::LOG_FILE_PATH);
+}
 
 $tmhOAuth = new tmhOAuth(array(
     'consumer_key'    => BotConfig::OAUTH_CONSUMER_KEY,
@@ -44,9 +50,15 @@ function streaming_callback($data, $length, $metrics) {
     if ($length == '0') {
         clog("NOP");
     } else {
-        clog("DATA");
+    	$data = json_decode($data);
+    	clog("DATA:");
+    	clog($data);
 
-        $tweet_id = json_decode($data);
+    	// If that tweet was already retweeted we should already have it in our TL
+    	if ($data->retweet_count > 0) {
+    		return;
+    	}
+
         $tweet_id = $tweet_id->id_str;
 
         $params = array(
@@ -56,8 +68,6 @@ function streaming_callback($data, $length, $metrics) {
         $tmhOAuthRetweet->request("POST", str_replace('{id}', $tweet_id, $retweet_method), $params);
         tmhUtilities::pr($tmhOAuthRetweet);
 
-        clog("DATA:");
-        clog($data);
     }
 	
     return file_exists(dirname(__FILE__) . '/STOP');
--- index.php
+++ index.php
@@ -1,1 +1,24 @@
 <?php
+
+/**
+ * This file is the index file and will display you the log generated from bot.php
+ * If this runs not on a local web server you should protect this file via some 
+ * kind of access control, e.g. htpasswd
+ */
+
+require 'log/PHPTail.php';
+require 'config.php';
+
+/**
+ * Initilize a new instance of PHPTail
+ * @var PHPTail
+ */
+
+$tail = new PHPTail(BotConfig::LOG_FILE_PATH);
+
+if(isset($_GET['ajax']))  {
+	echo $tail->getNewLines($_GET['lastsize'], $_GET['grep'], $_GET['invert']);
+	die();
+}
+
+$tail->generateGUI();
